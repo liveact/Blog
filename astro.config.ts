c90d060011266e4941ae96b805ffae5926f30c18
Yugali <yugali.ltt@gmail.com>
2026-03-23 21:26:09 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -26,7 +26,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // [Basic]
-  site: 'https://blog-5at.pages.dev',
+  site: 'https://blog.yugali.cn',
   // Deploy to a sub path
   // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
